08aa5370c8c8d7fc99c0a33098a964bf1844a9bd
Jakub Kret <kubolo.1977@wp.pl>
2021-08-27 22:07:31 +0200

    Added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@theme/theme';
 import GlobalStyle from './globalStyle';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <h1>siema</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/"></Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
