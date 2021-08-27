fe288a5a0d14a78eac4e192256ba17d808c0c03a
Jakub Kret <kubolo.1977@wp.pl>
2021-08-27 18:53:54 +0200

    Created order and user models

---
+++ src/utils/models/user.model.ts
@@ -0,0 +1,13 @@
+export interface User {
+    id?:number,
+    userName:string,
+    userLastName:string,
+    userCity:string,
+    userAdressStreetName:string,
+    userAdressStreetNumber:string,
+    userAdressHomeNumber:string,
+    userPhoneNumber?:string,
+    userUserName:string,
+    userPassword:string,
+    userType:string
+}
