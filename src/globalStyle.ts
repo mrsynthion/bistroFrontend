dabf3e9c1de992417844b8810514151e5b9e9844
Jakub Kret <kubolo.1977@wp.pl>
2021-09-02 18:42:46 +0200

    Enden up basic header implementation

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -3,13 +3,14 @@ import { createGlobalStyle } from "styled-components";
 export default createGlobalStyle`
 *,*::before,*::after{
     box-sizing: border-box;
-    font-family:'Lora','Monaco',sans-serif;
-    font-size:16px;
+
 }
 body{
     margin:0;
     padding:0;
     background-color: var(--orange-100);
+    font-size: 16px;
+    font-family:'Lora','Monaco',sans-serif;
 }
 `
 
